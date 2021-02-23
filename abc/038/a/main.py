10bbdb535f9302b7493eb379cf036544d0c5a7e5
Hiroshi Koyama <koyahi.dev@gmail.com>
2021-02-23 23:37:11 +0900

    Add main.py

--- abc/038/a/main.py
+++ abc/038/a/main.py
@@ -1,3 +1,2 @@
-H1, W1 = map(int, input().split())
-H2, W2 = map(int, input().split())
-print("YES" if H1 == W2 or H1 == H2 or W1 == W2 or W1 == H2 else "NO")
+S = input()
+print("YES" if S[-1] == "T" else "NO")
